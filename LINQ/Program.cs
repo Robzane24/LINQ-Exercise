7876da26bbabf7b71e40df270ee80dbe886276d7
Robert Zane <rob.zane24@gmail.com>
2022-12-20 11:22:11 -0600

    Completed LINQ Exercise one

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a list of video game names... done
+            //Order the list of games by length of the game name.
+            //Use the lambda expression in this exercise as well.
+            //use Method Syntax for this exercise
+
+            List<string> sportsTeams = new List<string>() { "Rangers", "Angels", "Indians", "Oilers", "Stars" };
+
+           var sortedList = sportsTeams.OrderBy(x => x.Length);
+
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
